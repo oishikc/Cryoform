a9a91bf2901cf00507fad44dde95d0e8138bd237
Oishik Chakraborty <oishikc18@gmail.com>
2026-06-01 20:27:23 -0400

    Create lipids.py

---
+++ data/lipids/lipids.py
@@ -0,0 +1,9 @@
+# SMILES for the four core LNP components
+# Source: verified from literature and PubChem cross-referenced entries
+
+LIPID_SMILES = {
+    "ALC-0315": "CCCCCCCCCCCC(CC(=O)OCC(COC(=O)CC(CCCCCCCCCCCC)N(CCCO)CCCO)OCCOCCOCCO)N(CCCO)CCCO",
+    "DSPC": "CCCCCCCCCCCCCCCCCC(=O)OC[C@H](COP(=O)([O-])OCC[N+](C)(C)C)OC(=O)CCCCCCCCCCCCCCCCC",
+    "Cholesterol": "C[C@H](CCCC(C)C)[C@H]1CC[C@@H]2[C@@]1(CC[C@H]3[C@H]2CC=C4[C@@]3(CC[C@@H](C4)O)C)C",
+    "ALC-0159": "CCCOCCOCCOCCOCCOCCOCCOCCOCCOCCOCCOCCOCCOCCOCCO",
+}
